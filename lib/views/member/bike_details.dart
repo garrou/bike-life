ede3d6ee578df400c5561cd45665eb7ccbd3d67b
Adrien <39885692+1-irdA@users.noreply.github.com>
2021-10-26 22:48:54 +0200

    Add components

--- lib/views/member/bike_details.dart
+++ lib/views/member/bike_details.dart
@@ -5,7 +5,6 @@ import 'package:bike_life/repositories/bike_repository.dart';
 import 'package:bike_life/views/styles/general.dart';
 import 'package:bike_life/views/widgets/button.dart';
 import 'package:bike_life/views/widgets/top_left_button.dart';
-import 'package:bike_life/views/widgets/title.dart';
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
@@ -46,18 +45,39 @@ class _BikeDetailsState extends State<BikeDetails> {
     }));
   }
 
-  Widget wideLayout() => Column(children: <Widget>[
+  Widget wideLayout() => Padding(
+      padding: const EdgeInsets.symmetric(horizontal: thirdSize),
+      child: ListView(children: <Widget>[
         AppTopLeftButton(callback: () => Navigator.pop(context)),
-        AppTitle(text: widget.bike.name, paddingTop: 0.0),
-        LinearPercentIndicator(
-            linearStrokeCap: LinearStrokeCap.roundAll,
-            percent: components != null
-                ? components!.frameKm / components!.frameDuration
-                : 0.0,
-            backgroundColor: mainColor,
-            progressColor: Colors.amber),
+        Center(child: Text(widget.bike.name, style: secondTextStyle)),
+        _buildPercentBar(
+            'Cadre', components!.frameKm, components!.frameDuration),
+        _buildPercentBar(
+            'Fourche', components!.forkKm, components!.forkDuration),
+        _buildPercentBar(
+            'Chaîne', components!.stringKm, components!.stringDuration),
+        _buildPercentBar('Transmission', components!.transmissionKm,
+            components!.transmissionDuration),
+        _buildPercentBar('Roue avant', components!.wheelForwardKm,
+            components!.wheelForwardDuration),
+        _buildPercentBar('Roue arrière', components!.wheelBackwardKm,
+            components!.wheelBackwardDuration),
+        _buildPercentBar('Pneu avant', components!.tireForwardKm,
+            components!.tireForwardDuration),
+        _buildPercentBar('Pneu arrière', components!.tireBackwardKm,
+            components!.tireBackwardDuration),
+        _buildPercentBar('Chambre à air avant', components!.airChamberForwardKm,
+            components!.airChamberForwardDuration),
+        _buildPercentBar(
+            'Chambre à air arrière',
+            components!.airChamberBackwardKm,
+            components!.airChamberBackwardDuration),
+        _buildPercentBar('Frein avant', components!.brakeForwardKm,
+            components!.brakeForwardDuration),
+        _buildPercentBar('Frein arrière', components!.brakeBackwardKm,
+            components!.brakeBackwardDuration),
         AppButton(text: 'Supprimer', callback: _onDeleteBike, color: errorColor)
-      ]);
+      ]));
 
   Widget narrowLayout() => Padding(
       padding: const EdgeInsets.symmetric(horizontal: maxPadding),
@@ -75,4 +95,21 @@ class _BikeDetailsState extends State<BikeDetails> {
       Navigator.pushNamed(context, '/tips');
     }
   }
+
+  Widget _buildPercentBar(String label, double km, double duration) => Padding(
+      padding: const EdgeInsets.only(top: thirdSize),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(label, style: thirdTextStyle),
+          LinearPercentIndicator(
+              center:
+                  Text(components != null ? km.toString() + " km" : "0.0 km"),
+              lineHeight: secondSize,
+              linearStrokeCap: LinearStrokeCap.roundAll,
+              percent: components != null ? km / duration : 0.0,
+              backgroundColor: mainColor,
+              progressColor: Colors.amber)
+        ],
+      ));
 }
